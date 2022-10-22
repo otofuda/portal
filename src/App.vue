1f8aff5bced5bbb452ac526bb7bd845c27ce6717
mtsgi <oq@live.jp>
2022-10-22 17:51:35 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <a href="https://www.ne.senshu-u.ac.jp/~proj2019-13/">
+      <img src="./assets/logo.png" class="logo" alt="音札" />
+    </a>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  width: 10em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 1em #64bcff88);
+}
+</style>
