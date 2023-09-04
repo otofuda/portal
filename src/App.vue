e88deb7e7a1166cc539f6d4b81ef0d10a85c3a1a
mtsgi <oq@live.jp>
2023-09-05 00:43:43 +0900

    add links

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,49 @@
 </script>
 
 <template>
-  <div>
+  <main>
+    <img src="./assets/logo.png" class="logo" alt="音札" />
     <a href="https://www.ne.senshu-u.ac.jp/~proj2019-13/">
-      <img src="./assets/logo.png" class="logo" alt="音札" />
+      <img src="./assets/banner_hp.png" class="link" alt="音札公式ホームページ" />
     </a>
-  </div>
+    <a href="https://etude.otofuda.com">
+      <img src="./assets/banner_etude.png" class="link" alt="音札Étude" />
+    </a>
+    <a href="https://twitter.com/otofuda" target="_blank">
+      <img src="./assets/banner_twitter.png" class="link s" alt="Twitter" />
+    </a>
+    <a href="https://j.mp/otfdyt" target="_blank">
+      <img src="./assets/banner_youtube.png" class="link s" alt="YouTube" />
+    </a>
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  width: 10em;
-  padding: 1.5em;
-  will-change: filter;
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1em;
 }
-.logo:hover {
+
+.logo {
+  width: 8em;
+  margin-bottom: 3em;
+}
+
+.link {
+  width: 25em;
+  max-width: 100%;
+  will-change: filter;
+  transition: 0.5s filter ease;
+  filter: drop-shadow(0 0 1em #64bcff00);
+}
+
+.link:hover {
   filter: drop-shadow(0 0 1em #64bcff88);
+}
+
+.link.s {
+  width: 15em;
 }
 </style>
